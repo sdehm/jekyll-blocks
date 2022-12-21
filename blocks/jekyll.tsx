5b044a9314b861f01ab99591b3810984a33cbc62
Shaffer Dehmlow <sdehm@github.com>
2022-12-21 13:12:05 -0700

    Scaffold new block type

---
+++ blocks/jekyll.tsx
@@ -0,0 +1,8 @@
+import { FolderBlockProps } from "@githubnext/blocks";
+import { Box } from "@primer/react";
+
+export default function (props: FolderBlockProps) {
+  return (
+    <p>Hello World!</p>
+  );
+}
