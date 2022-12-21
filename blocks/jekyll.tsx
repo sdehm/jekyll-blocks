a20a360037754351f25fd7dc3fd47b89df8961b7
Shaffer Dehmlow <sdehm@github.com>
2022-12-21 15:48:42 -0700

    Add initial version of jekyll block

--- blocks/jekyll.tsx
+++ blocks/jekyll.tsx
@@ -1,8 +1,36 @@
-import { FolderBlockProps } from "@githubnext/blocks";
-import { Box } from "@primer/react";
+import { FolderBlockProps, FileBlockProps } from "@githubnext/blocks";
+import { useEffect, useMemo, useState } from "react";
+import DOMPurify from "dompurify";
 
 export default function (props: FolderBlockProps) {
+  const { tree, onRequestGitHubData } = props;
+  const content = tree.find((item) => item.path === "src/_site/index.html");
+
+  const [sanitizedContent, setSanitizedContent] = useState<string>("");
+
+  const getContent = async () => {
+    const file = await onRequestGitHubData(content?.url as string);
+    const file_content = file.content;
+    const sanitizedContent = DOMPurify.sanitize(atob(file_content));
+    setSanitizedContent(sanitizedContent);
+  };
+
+  useEffect(() => {
+    getContent();
+  }, []);
+
   return (
-    <p>Hello World!</p>
+    <div
+      style={{
+        padding: "25px 20px",
+      }}
+    >
+      <div
+        style={{
+          all: "initial",
+        }}
+        dangerouslySetInnerHTML={{ __html: sanitizedContent }}
+      />
+    </div>
   );
 }
